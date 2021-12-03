c5a3b8e1d3725789c9dd794f6552377409ddc273
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2021-12-03 14:29:55 +0100

    ordenar y funcion camino

--- laberinto.py
+++ laberinto.py
@@ -6,6 +6,14 @@ laberinto = [
     ['a', 'a', 'a', 'a', 'a']
     ]
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+camino = []
+
+
+def meterEnLista(numA, numB, lista):
+    par = []
+    par.append(numA)
+    par.append(numB)
+    lista.append(par)
 
 def es_muro(numA, numB):
     for i in range(len(muro)):
@@ -16,14 +24,33 @@ def es_muro(numA, numB):
     return False
 
 
-for i in range(len(laberinto)):
-    for j in range(len(laberinto[0])):
-        #print(str(i) + ' ' + str(j))
-        if es_muro(i, j) == True: 
-            laberinto[i][j] = 'X'
-        else:
-            laberinto[i][j] = ' '
-laberinto[4][4] = 'S'
+def init_laberinto():
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto[0])):
+            #print(str(i) + ' ' + str(j))
+            if es_muro(i, j) == True: 
+                laberinto[i][j] = 'X'
+            else:
+                laberinto[i][j] = ' '
+    laberinto[4][4] = 'S'
+
+
+
+def init_camino():
+    for i in range(len(laberinto)):
+        for j in range(len(laberinto)):
+            if laberinto[i][j] == ' ':
+                meterEnLista(i, j, camino)
+            elif laberinto[i][j] == 'S':
+                meterEnLista(i, j, camino)
+
+       
+
+init_laberinto()
+init_camino()
 
 for i in range(len(laberinto)):
     print(laberinto[i])
+
+print(camino)
+            
